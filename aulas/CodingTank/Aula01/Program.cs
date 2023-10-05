99ffcdc1ab29afc838828b99df610daee06e5158
Felipe Augusto <augusto_felipe@icloud.com>
2023-10-04 22:13:12 -0300

    aula01

---
+++ aulas/CodingTank/Aula01/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Aula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.Write("Digite o seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"Olá, {nome}! Bem-vindo ao Coding Tank.");
+
+            string idadeString = Console.ReadLine();
+
+            // TRY CATCH
+            try
+            {
+                int idade = int.Parse(Console.ReadLine());  
+            }
+            catch
+            {
+                Console.WriteLine("Não foi possível coverter.");
+            }
+
+
+            // TRY PARSE
+            if (int.TryParse(idadeString, out int numero))
+            {
+                Console.WriteLine($"Voce digitou um número {numero}");
+            }
+            else
+            {
+                Console.WriteLine("Voce nao digitou um número.");
+            }
+        }
+    }
+}
